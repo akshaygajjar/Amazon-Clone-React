be0e89ac55be730d4ddb093855eaf4dad255dd62
Akshay Gajjar <akshaygajjar101@gmail.com>
2021-07-31 20:15:43 +0530

    User Authentication Using Firebase

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyB5CUb4zb9GTsqKe6bhpb-l19bjG2YEuFs',
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: '1:23714024396:web:2334fd29bbdc445cfa6b69',
   measurementId: 'G-SMWGRFTBYX',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
