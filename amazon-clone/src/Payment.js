b0d01bdf36a4428da187783503c622f7452d2fd7
Akshay Gajjar <akshaygajjar101@gmail.com>
2021-08-01 16:15:26 +0530

    Make a payment page component and style it

---
+++ amazon-clone/src/Payment.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './Payment.css';
+import CheckoutProduct from './CheckoutProduct';
+import CurrencyFormat from 'react-currency-format';
+import { getCartTotal } from './reducer';
+import { Link, useHistory } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+function Payment() {
+  const [{ cart, user }, dispatch] = useStateValue();
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        <h1>
+          Checkout (<Link to='/checkout'>{cart?.length} items</Link>)
+        </h1>
+
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+            <p>71, React Lane</p>
+            <p>DAICT, Gandhinagar</p>
+          </div>
+        </div>
+
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className='payment__items'>
+            {cart.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Payment Method</h3>
+          </div>
+          <div className='payment__details'>
+            <form>
+              <CardElement />
+
+              <div className='payment__priceContainer'>
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getCartTotal(cart)}
+                  displayType={'text'}
+                  thousandSeparator={true}
+                  prefix={'$'}
+                />
+                <button>
+                  <span>Buy Now</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import { useHistory } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
 import { getCartTotal } from './reducer';
 
 function Subtotal() {
+  const history = useHistory();
   const [{ cart }, dispatch] = useStateValue();
 
   return (
@@ -27,7 +29,9 @@ function Subtotal() {
         prefix={'$'}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push('/payment')}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
